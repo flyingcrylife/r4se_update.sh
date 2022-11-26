f3a444327a9e3e58d71b5e7b9356b33af2bfdca3
flyingcrylife <38677562+flyingcrylife@users.noreply.github.com>
2022-11-26 15:43:52 +0800

    Update r4se_update.sh

--- r4se_update.sh
+++ r4se_update.sh
@@ -25,7 +25,7 @@ hd_check () {
 #设备选择
 machine_choose () {
     echo -e '\e[92m输入对应数字选择设备或退出\e[0m'
-    echo -e '0 --- 退出\n1 --- R4S\n2 --- DN2\n3 --- X86'
+    echo -e '0 --- 退出\n1 --- R4SE\n2 --- DN2\n3 --- X86'
     read -p "请输入数字[0-3],回车确认 " machine_num
     case $machine_num in
         0)
